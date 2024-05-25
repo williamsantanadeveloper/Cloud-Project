3ecfb35975a3019a410989f2d88ec11b428e58a8
Antonio Fernandes <devantonio.fer@gmail.com>
2024-05-25 12:42:36 -0300

    fix 10: deploy

--- aplicacao_cloud/urls.py
+++ aplicacao_cloud/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', lambda request: redirect('admin/', permanent=True)),
     path('admin/', admin.site.urls)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
