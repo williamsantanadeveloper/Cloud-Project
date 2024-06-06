a0302010098cfa4db4bfd2dfbaa0fb194ad864e1
William Santana <71521910+williamsantanadeveloper@users.noreply.github.com>
2024-06-06 10:03:36 -0300

    Update models.py

--- receitas/models.py
+++ receitas/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class Tag(models.Model):
     nome = models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return f"{self.id} - {self.nome}"
+
 
 class Receita(models.Model):
     titulo = models.CharField(max_length=255, unique=True)
     descricao = models.TextField()
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f"{self.id} - {self.nome}"
